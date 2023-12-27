51b3b722f2d013e274ed7f63cca453ab66fe281c
Jack Hickson <jack.r.hickson@gmail.com>
2023-12-26 23:11:14 -0500

    initial firebase implementation

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from '../firebaseConfig'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const app = initializeApp(firebaseConfig);
+
+export const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
+
